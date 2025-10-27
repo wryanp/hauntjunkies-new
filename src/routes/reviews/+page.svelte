b6ac7bcadb185d0e45d8c3db3d70b3e27e217848
Vilonte McCloud <vilontemccloud@Vilontes-New-MacBook-Pro.local>
2025-10-27 09:22:54 -0400

    Show city and full state name on review cards

--- src/routes/reviews/+page.svelte
+++ src/routes/reviews/+page.svelte
@@ -209,12 +209,12 @@
 											<h4 class="text-2xl font-bold text-white group-hover:text-haunt-orange transition-colors mb-2">
 												{review.name}
 											</h4>
-											{#if review.state}
+											{#if review.city && review.state}
 												<p class="text-gray-400 mb-3 flex items-center gap-1">
 													<svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
 														<path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
 													</svg>
-													{getStateName(review.state)}
+													{review.city}, {getStateName(review.state)}
 												</p>
 											{/if}
 											{#if review.rating_overall}
